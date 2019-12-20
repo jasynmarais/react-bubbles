039f070603978b313ce2e599df230e3fed0fd9d5
Jasyn Marais <jasynmarais@gmail.com>
2019-12-20 12:12:44 +0200

    Change username and password

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,14 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import axiosWithAuth from '../utils/axiosWithAuth';
 
-import Bubbles from "./Bubbles";
-import ColorList from "./ColorList";
+import Bubbles from './Bubbles';
+import ColorList from './ColorList';
 
 const colorsURL = 'http://localhost:5000/api/colors';
 
-const BubblePage = () => {
+const BubblePage = props => {
   const [colorList, setColorList] = useState([]);
-  
+  // const [isFetching, setIsFetching] = useState(false);
+
+  const onLogout = () => {
+    localStorage.clear();
+    props.history.push('/');
+  };
 
   useEffect(() => {
     axiosWithAuth()
@@ -21,8 +26,17 @@ const BubblePage = () => {
 
   return (
     <>
-      <ColorList colors={colorList} updateColors={setColorList} />
-      <Bubbles colors={colorList} />
+      {/*isFetching ? (
+        <p>Loading...</p>
+      ) : ()*/}
+      <>
+        <ColorList
+          colors={colorList}
+          updateColors={setColorList}
+          onLogout={onLogout}
+        />
+        <Bubbles colors={colorList} />
+      </>
     </>
   );
 };
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -16,20 +16,20 @@ const Login = props => {
   };
 
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
+    <div className='login-container'>
+      <h1 className='title'>Welcome to the Bubble App!</h1>
       <Formik
         initialValues={{ username: '', password: '' }}
         onSubmit={onLogin}
         render={() => (
           <Form className='login'>
-            <Field name='username' type='text' placeholder='username' />
-            <Field name='password' type='text' placeholder='password' />
-            <input type='Submit' />
+            <Field name='username' type='text' placeholder='Username' />
+            <Field name='password' type='text' placeholder='Password' />
+            <input className='button submit-button' type='Submit' />
           </Form>
         )}
-        />
-    </>
+      />
+    </div>
   );
 };
 
